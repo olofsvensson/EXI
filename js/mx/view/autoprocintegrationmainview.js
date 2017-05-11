b04c2be398fcd7d2ebb6ecdbed312a58d5577c6a
Alejandro De Maria Antolinos <demariaa@esrf.fr>
2017-05-11 15:24:31 +0200

    Fixed problem with autoprocessing plots. It closes #213

--- js/mx/view/autoprocintegrationmainview.js
+++ js/mx/view/autoprocintegrationmainview.js
@@ -11,7 +11,7 @@ function AutoProcIntegrationMainView() {
 	var _this = this;
     this.id = BUI.id();
 
-	this.autoProcIntegrationGrid = new AutoProcIntegrationGrid({height:310});
+	this.autoProcIntegrationGrid = new AutoProcIntegrationGrid({height:310});    
     this.autoProcIntegrationPlots = new AutoProcIntegrationPlots();
     this.autoProcIntegrationAttachmentsGrid = new AutoProcIntegrationAttachmentGrid();
 }
@@ -21,7 +21,7 @@ AutoProcIntegrationMainView.prototype.getPanel = MainView.prototype.getPanel;
 
 AutoProcIntegrationMainView.prototype.getContainer = function() {
 
-    hPanel = Ext.create('Ext.container.Container', {
+    var hPanel = Ext.create('Ext.container.Container', {
         margin : 20,
         cls : 'border-grid',
         layout : 'hbox',
@@ -43,24 +43,6 @@ AutoProcIntegrationMainView.prototype.getContainer = function() {
 };
 
 /**
-* It loads the list of attachments and stores them into two class variables:
-* _this.programAttachmentsAutoProcProgramIds with the ids of the autoprocprograms
-* _this.programAttachments with a list of attachments
-* @method loadAttachments
-*/
-/*
-AutoProcIntegrationMainView.prototype.loadAttachments = function(autoProcessingIntegrationList) {
-    var _this = this;
-    
-  
-	var onSuccess = function(sender, data){
-        _this.programAttachments = (data);
-	};
-    _this.programAttachmentsAutoProcProgramIds = _.map(autoProcessingIntegrationList, 'v_datacollection_summary_phasing_autoProcProgramId');
-	EXI.getDataAdapter({onSuccess : onSuccess}).mx.autoproc.getAttachmentListByautoProcProgramsIdList(_this.programAttachmentsAutoProcProgramIds);
-};	
-*/
-/**
 * It loads autoproc.getViewByDataCollectionId from autoprocessingdataadapter and call to loadAttachments
 * @method load
 */
@@ -71,18 +53,15 @@ AutoProcIntegrationMainView.prototype.load = function(data) {
 	this.autoProcIntegrationGrid.load(data);
     this.autoProcIntegrationPlots.load(data);
 
-    if (data.length == 1) {
-        this.autoProcIntegrationAttachmentsGrid.hide(false);
+    if (data.length == 1) {      
         this.autoProcIntegrationAttachmentsGrid.panel.setLoading();
-        var onSuccess = function (sender,attachments) {
+        var onSuccess = function (sender,attachments) {            
             _this.autoProcIntegrationAttachmentsGrid.panel.setLoading(false);
             if (attachments){
                 _this.autoProcIntegrationAttachmentsGrid.load(_.uniqBy(_.flatten(attachments),"autoProcProgramAttachmentId"));
             }
-        } 
+        }         
         EXI.getDataAdapter({onSuccess : onSuccess}).mx.autoproc.getAttachmentListByautoProcProgramsIdList(_.map(data,'v_datacollection_summary_phasing_autoProcProgramId'));
-    } else {
-        this.autoProcIntegrationAttachmentsGrid.hide(true);
     }
 };
 
