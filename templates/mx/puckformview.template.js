3a31f83d7bd99c10f289b7b10b7acd964aafd1d2
Alejandro De Maria Antolinos <demariaa@esrf.fr>
2017-05-30 09:50:41 +0200

    It fixes issue_222

--- templates/mx/puckformview.template.js
+++ templates/mx/puckformview.template.js
@@ -1,21 +1,17 @@
 
 
-<div class="container"> 
-  <div class="row ">
-        <div class="col-sm-6">
-            <div id='{.specialCharacterInfoPanelId}' class="alert alert-info" style='margin-bottom: 1px;height: 30px;  line-height:30px;  padding:0px 15px;display:inline-block;margin:20px;' role="alert">
+
+            <div id='{.specialCharacterInfoPanelId}' class="alert alert-info" style='margin-bottom: 1px;height: 20px;  line-height:20px;  padding:0px 5px 0px 10px;display:inline-block;margin:5px;' role="alert">
                 <span class="glyphicon glyphicon-warning-sign" aria-hidden="true"> </span> 
                 Sample name field is mandatory and no special characters are allowed
             </div>
-        </div>
-        <div class="col-sm-6">
-            <div id='{.uniquenessInfoPanelId}' class="alert alert-info" style='margin-bottom: 1px;height: 30px;  line-height:30px;  padding:0px 15px;display:inline-block;margin:20px;' role="alert">
+
+            <br />
+
+            <div id='{.uniquenessInfoPanelId}' class="alert alert-info"  style='margin-bottom: 1px;height: 20px;  line-height:20px;  padding:0px 5px 0px 10px;display:inline-block;margin:5px;' role="alert">
                 <span class="glyphicon glyphicon-warning-sign" aria-hidden="true"></span> 
                 Protein + sample name should be unique for the whole proposal
             </div>
-        </div>
-  </div>
-</div>
 
 
 
