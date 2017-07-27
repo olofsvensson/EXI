60862bba8b08cb9dc9bfc5f094a9582003fa1b61
Alejandro De Maria Antolinos <demariaa@esrf.fr>
2017-07-27 16:12:10 +0200

    Filepath is not relative to /data/visitor

--- templates/saxs/measurement.grid.template.js
+++ templates/saxs/measurement.grid.template.js
@@ -1,21 +1,21 @@
 <div class="container-fluid persist-header" align="center">
     <div class="row text-center" style="border : 1px solid #ccc;">
-        <div class="col-sm-3 queue-border-right-columns ">
+        <div class="col-sm-2 queue-border-right-columns ">
             <div>&nbsp;</div>
             Specimen
             <div class="row text-center">
                 <div class="col-sm-4 queue-border-top-columns queue-border-right-columns">
                     Macromolecule
                 </div>
-                <div class="col-sm-4 queue-border-top-columns queue-border-right-columns">
+                <div class="col-sm-2 queue-border-top-columns queue-border-right-columns">
                     Conc.
                 </div>
-                <div class="col-sm-4 queue-border-top-columns">
+                <div class="col-sm-6 queue-border-top-columns">
                     Buffer
                 </div>
             </div>
         </div> 
-        <div class="col-sm-5 queue-border-right-columns">
+        <div class="col-sm-4 queue-border-right-columns">
             <div>&nbsp;</div>
             Parameters
             <div class="row text-center">
@@ -39,12 +39,8 @@
                 </div>
             </div>
         </div> 
-        <div class="col-sm-1 queue-border-right-columns" >
-            <div>&nbsp;</div>
-            Status
-            <div>&nbsp;</div>
-        </div>
-        <div class="col-sm-3 queue-border-right-columns" >
+       
+        <div class="col-sm-6 queue-border-right-columns" >
             <div>&nbsp;</div>
             Average File
             <div>&nbsp;</div>
@@ -58,20 +54,20 @@
     {#.}
     <div class="container-fluid persist-header">
         <div class="row queue-border-bottom-columns" {?Run_runId}style="background:rgb(188,245,169);"{/Run_runId}>
-            <div class="col-sm-3 ">
+            <div class="col-sm-2 ">
                 <div class="row">
                     <div class="col-sm-4">
                         {.Macromolecule_acronym}
                     </div>
-                    <div class="col-sm-4">
+                    <div class="col-sm-2">
                         {.Specimen_concentration}
                     </div>
-                    <div class="col-sm-4 ">
+                    <div class="col-sm-6 ">
                         {.Buffer_acronym}<span style='font-style:oblique;'> Plate: [{.SamplePlate_slotPositionColumn},{.samplePlateLetter}-{.SamplePlatePosition_columnNumber}]</span>
                     </div>
                 </div>
             </div> 
-            <div class="col-sm-5 ">
+            <div class="col-sm-4 ">
                 <div class="row">
                     <div class="col-sm-2 ">
                         {.Run_exposureTemperature}
@@ -91,12 +87,11 @@
                     <div class="col-sm-2 ">
                         {.Measurement_viscosity}
                     </div>
+                    
                 </div>
             </div> 
-            <div class="col-sm-1 ">
-                <span style='font-weight: bold;'>{?Run_runId}DONE{/Run_runId}</span>
-            </div>
-            <div class="col-sm-3 ">
+           
+            <div class="col-sm-6 ">
                 <span style='font-weight: bold;'>{.Merge_averageFilePath}</span>
             </div>
             
