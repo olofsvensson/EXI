b0a05375b11d6c114f938d22ae1e443525694504
Alejandro De Maria Antolinos <demariaa@esrf.fr>
2017-11-30 15:07:05 +0100

    Using grid for rendering the moview

--- templates/em/datacollectionsummary/em.second.general.mxdatacollectiongrid.template.js
+++ templates/em/datacollectionsummary/em.second.general.mxdatacollectiongrid.template.js
@@ -1,18 +1,29 @@
-<table class="table"> 
-     <tr>
-        <td></td>        
-        <td>Movies</td>
-        <td>Motion Correction</td>
-        <td> CTF</td>
-    </tr>               
-    {#gridSquares}
-    <tr>
-        <td  class='column_parameter_value'><a href='#/em/datacollection/{.DataCollection_dataCollectionId}/main'>Grid Square #{.name}</a></td>        
-        <td  class='column_parameter_value text-center'>{.movieCount}</td>
-        <td  class='column_parameter_value text-center'>{.motionCorrectionCount}</td>
-        <td  class='column_parameter_value text-center'>{.ctfCount}</td> 
-        
-    </tr>
-    {/gridSquares}   
-</table> 
+
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-1"></div>
+        <div class="col-sm-1">Movies</div> 
+        <div class="col-sm-1">Motion Correction</div> 
+        <div class="col-sm-1">CTF</div> 
+        <div class="col-sm-1">Img</div> 
+    </div> 
+     {#gridSquares} 
+     <div class="row">
+        <div class="col-sm-1"><a href='#/em/datacollection/{.dataCollectionId}/main'>Grid Square #{.name}</a></div>
+        <div class="col-sm-1">{.movieCount}</div> 
+        <div class="col-sm-1">{.motionCorrectionCount}</div> 
+        <div class="col-sm-1">{.ctfCount}</div> 
+        <div class="col-sm-1">
+            <a href="{.snapshot}" data-lightbox='{.snapshot}' data-title="{.name}">
+                <img  src="{.snapshot}" class='img-thumbnail' style='height:50px;width:250px;' data-src="{.snapshot}"/>                  
+            </a>                 
+        </div> 
+{.snapshot}
+      </div>
+
+      {/gridSquares}   
+</div> 
+
+
 
