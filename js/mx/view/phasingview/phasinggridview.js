b87d85827cc82076d5f918509f163cec907b504f
Olof Svensson <svensson@linsvensson.esrf.fr>
2020-12-15 15:35:32 +0100

    Issue #525: Improvements of display of LIGAND_FIT results

--- js/mx/view/phasingview/phasinggridview.js
+++ js/mx/view/phasingview/phasinggridview.js
@@ -167,7 +167,7 @@ PhasingGridView.prototype.printHTML = function(target) {
 	                                    toBePushed["uglymol"] = '../viewer/uglymol/index.html?pdb=' + pdbUrl + '&map1=' + mapUrl1 + '&map2=' + mapUrl2;
 	                                }
 	                            }
-                            } else if (step == "REFINEMENT") {
+                            } else if ((step == "REFINEMENT") || (step == "LIGAND_FIT")) {
                                 var refinedPdbFileId = -1;
                                 var mrPdbFileId = -1;
                                 var peaksFileId = -1;
@@ -264,13 +264,16 @@ PhasingGridView.prototype.printHTML = function(target) {
                
                /**
                 StepS for Phasing are: PREPARE,  SUBSTRUCTUREDETERMINATION, PHASING AND MODELBUILDING
-                StepS for Molecular replacement are: PHASING AND REFINEMENT */
+                StepS for Molecular replacement are: PHASING, REFINEMENT and LIGAND_FIT*/
 
                /** If there is model building then we will display modelbuilding */
                if (_.find(stepsBySpaceGroup, {"PhasingStep_phasingStepType" : "MODELBUILDING"}) != null){
                         node = getNodeByPhasingStep(node, stepsBySpaceGroup, "MODELBUILDING");
                }
                else{
+                   if (_.find(stepsBySpaceGroup, {"PhasingStep_phasingStepType" : "LIGAND_FIT"}) != null){
+                       node = getNodeByPhasingStep(node, stepsBySpaceGroup, "LIGAND_FIT");                                  
+                   } else
                    if (_.find(stepsBySpaceGroup, {"PhasingStep_phasingStepType" : "REFINEMENT"}) != null){
                        node = getNodeByPhasingStep(node, stepsBySpaceGroup, "REFINEMENT");                                  
                    }
